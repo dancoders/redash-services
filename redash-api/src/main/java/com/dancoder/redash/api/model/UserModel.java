eff5c70f3e127599b604609a20128c1bb4e58f47
goudanz <dancoder@163.com>
2020-08-13 00:17:36 +0800

    commit jsonTypeHandlerPg & update userModel fields

--- redash-api/src/main/java/com/dancoder/redash/api/model/UserModel.java
+++ redash-api/src/main/java/com/dancoder/redash/api/model/UserModel.java
@@ -1,6 +1,10 @@
 package com.dancoder.redash.api.model;
 
 import lombok.Data;
+import org.json.simple.JSONObject;
+
+import java.sql.Date;
+import java.sql.Timestamp;
 
 /**
  * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
@@ -8,9 +12,15 @@ import lombok.Data;
 @Data
 public class UserModel {
     private Long id;
-
+    private Long orgId;
     private String name;
-
-    private Integer age;
-
+    private String email;
+    private String profileImageUrl;
+    private String passwordHash;
+    private Long groups;
+    private String apiKey;
+    private Date disabledAt;
+    private JSONObject details;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
 }
---
+++ redash-manager/src/main/java/com/dancoder/redash/common/JSONTypeHandlerPg.java
@@ -0,0 +1,40 @@
+package com.dancoder.redash.common;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedTypes;
+import org.postgresql.util.PGobject;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@MappedTypes(Object.class)
+public class JSONTypeHandlerPg extends BaseTypeHandler<Object> {
+
+    private static final PGobject jsonObject = new PGobject();
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
+        jsonObject.setType("json");
+        jsonObject.setValue(parameter.toString());
+        ps.setObject(i, jsonObject);
+    }
+
+    @Override
+    public Object getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return rs.getString(columnIndex);
+    }
+
+    @Override
+    public Object getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return cs.getString(columnIndex);
+    }
+
+    @Override
+    public Object getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return rs.getString(columnName);
+    }
+
+}
